10871d9fe2de714d1d4f2bc03c364aba7d75643e
Lucas <83518257+luqaska@users.noreply.github.com>
2022-03-15 23:52:23 -0300

    Actualización con el fin de reducir el tamaño del script

--- script.js
+++ script.js
@@ -3,49 +3,55 @@ title: script
 ---
 var num="";
 var last=num;
-var v1="<video autoplay loop muted><source src='";
-var v2="' type='video/";
-var v3="' /></video>";
-var i1="<img src='";var i2="' alt='IMG' />";
 /*var x=setInterval(function(){[lo de abajo]}, 1000);*/
+function vid(src, format) {
+	return "<video autoplay loop muted><source src='" + src + "' type='video/" + format + "' /></video>";
+}
+function img(src) {
+	return "<img src='" + src + "' alt='IMG' />";
+}
+function bkg(source, code) {
+	document.getElementById("link").href = source;
+	document.getElementById("bkg").innerHTML = code;
+}
 function setBkg() {
 	num=Math.floor(Math.random()*5);
 	if(num==0){
 		if(num!=last){
-			document.getElementById("link").href="https://pixabay.com/videos/id-19308/";
-			document.getElementById("bkg").innerHTML=v1+"/imgs/rio.mp4"+v2+"mp4"+v3;
+			bkg("https://pixabay.com/videos/id-19308/",
+			vid("/imgs/rio.mp4", "mp4"));
 			last=num;
 		}else{
 			setBkg();
 		}
 	}else if(num==1){
 		if(num!=last){
-			document.getElementById("link").href="https://youtu.be/bJPX5DPsRck";
-			document.getElementById("bkg").innerHTML=v1+"/imgs/shroomjak.mp4' type='video/mp4' /></video>";
+			bkg("https://youtu.be/bJPX5DPsRck",
+			vid("/imgs/shroomjak.mp4", "mp4"));
 			last=num;
 		}else{
 			setBkg();
 		}
 	}else if(num==2){
 		if(num!=last){
-			document.getElementById("link").href="https://pixabay.com/photos/clouds-sky-atmosphere-blue-sky-7050884/";
-			document.getElementById("bkg").innerHTML=i1+"https://pixabay.com/images/download/clouds-7050884_640.jpg"+i2;
+			bkg("https://pixabay.com/photos/clouds-sky-atmosphere-blue-sky-7050884/",
+			img("https://pixabay.com/images/download/clouds-7050884_640.jpg"));
 			last=num;
 		}else{
 			setBkg();
 		}
 	}else if(num==3){
 		if(num!=last){
-			document.getElementById("link").href="https://pixabay.com/photos/id-2528359/";
-			document.getElementById("bkg").innerHTML=i1+"/imgs/carpincho.jpg"+i2;
+			bkg("https://pixabay.com/photos/id-2528359/",
+			img("/imgs/carpincho.jpg"));
 			last=num;
 		}else{
 			setBkg();
 		}
 	}else if(num==4){
 		if(num!=last){
-			document.getElementById("link").href="https://www.redbubble.com/es/people/triinamariia/works/15636707-graphic-design-is-my-passion-rainbow-comic-sans";
-			document.getElementById("bkg").innerHTML=i1+"https://ih0.redbubble.net/image.82233968.6707/flat,800x800,075,f.jpg"+i2;
+			bkg("https://www.redbubble.com/es/people/triinamariia/works/15636707-graphic-design-is-my-passion-rainbow-comic-sans",
+			img("https://ih0.redbubble.net/image.82233968.6707/flat,800x800,075,f.jpg"));
 			last=num;
 		}else{
 			setBkg();
